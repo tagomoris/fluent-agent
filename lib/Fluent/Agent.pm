d3d82e0eb29883ae9d487f652b73a07335c72b54
TAGOMORI Satoshi <tagomoris@gmail.com>
2012-09-01 19:27:31 +0900

    writing: first step of bin/fluent-agent options/settings

--- lib/Fluent/Agent.pm
+++ lib/Fluent/Agent.pm
@@ -1,17 +1,26 @@
-package Fluent::Agent 0.0.1;
+package Fluent::Agent v0.0.1;
 
-use 5.12.0;
+use 5.14.0;
 use Carp;
 
 use Time::Piece;
 use Time::HiRes;
 use Log::Minimal;
 
-use UV;
+# use UV; mmm...
 
 use IO::Socket::INET;
 use Data::MessagePack;
 use JSON::XS;
 
+sub new {
+    # input, output, filter, ping, buffers
+    my ($this, %args) = @_;
+}
+
+sub execute {
+    # checker (term, reload)
+    my ($self, %args) = @_;
+}
 
 1;
